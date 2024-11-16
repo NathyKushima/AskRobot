d09dce21f39eac8875bc1f38507905b17b752c9c
NathyKushima <nhkushima@gmail.com>
2024-11-16 01:13:16 -0300

    ✅ only with functional views

--- PostsRobots/views.py
+++ PostsRobots/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .models import Posts
+from .forms import PostsForm
 
-# Create your views here.
+def post_about(request):
+    return render(request, 'PostsRobots/about.html')
+
